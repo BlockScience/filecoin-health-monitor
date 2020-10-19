35613e6d798462b8c8a5fcd1611bea4d290c9d39
Danilo Lessa Bernardineli <danilo.lessa@gmail.com>
2020-10-19 16:53:16 -0300

    added sentinel diagram | changed README

--- notebooks/query.py
+++ notebooks/query.py
@@ -27,18 +27,15 @@ with open(CONN_STRING_PATH, 'r') as fid:
 connection = create_engine(conn_string, pool_recycle=3600).connect()
 QUERY = """
         SELECT 
-        avg(ce.circulating_fil::NUMERIC) / 1e18 AS circulating_fil,
-        avg(ce.vested_fil::NUMERIC) / 1e18 AS vested_fil,
-        avg(ce.mined_fil::NUMERIC) / 1e18 AS mined_fil,
-        avg(ce.burnt_fil::NUMERIC) / 1e18 AS burnt_fil,
-        avg(ce.locked_fil::NUMERIC) / 1e18 AS locked_fil,
-        min(to_timestamp(b.timestamp)) AS time
-        FROM chain_economics ce
-        LEFT JOIN block_headers b 
-        ON b.parent_state_root = ce.parent_state_root
-        GROUP BY ce.parent_state_root
-        ORDER BY time ASC
-        LIMIT 100
+        total_qa_bytes_power::numeric * 2^(-50) AS qaNP,
+        total_qa_bytes_committed::numeric * 2^(-50) as qaNP_committed,
+        qa_smoothed_position_estimate::numeric * 2^(-128) * 2^(-50) AS qaNP_estimate,
+        bh.timestamp
+        FROM chain_powers cp
+        LEFT JOIN block_headers bh
+        ON bh.parent_state_root = cp.state_root
+        ORDER BY bh.timestamp ASC
+        limit 1000
         """
 
 df = (pd.read_sql(QUERY, connection)
